fff5bedd95bad447dd190e389b8efbf270fdf520
apfestin <festinadelen@gmail.com>
2015-10-30 16:38:44 +0800

    Set angular delimeter to {[ ]}

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
-app = angular.module('promosApp', []);
+var app = angular.module('promosApp', [], function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[');
+    $interpolateProvider.endSymbol(']}');
+});
 
 app.controller('PromosCtrl', [function() {
 
